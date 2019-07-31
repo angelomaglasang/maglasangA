0866bcb8e63c79fe560140b600d7da95f0d20998
angelomaglasang <53499928+angelomaglasang@users.noreply.github.com>
2019-07-31 09:48:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html>
+
 <head>
-<h1>Hello World</h1>
+<h1>This is the header</h1>
 </head>
+
 <body>
-<h2>Hello! Im here to help</h1>
+
+<main>
+  <h1>main</h1>
+  <p>This is the main part of the body in html </p>
+</main>
+
+<footer>
+  <p>Posted by: Ur Student</p>
+  <p>Contact information: <a href="mailto:someone@example.com">angelomaglasang.com</a>.</p>
+</footer>
+
 </body>
 </html>
