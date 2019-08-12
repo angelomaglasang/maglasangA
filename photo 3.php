39aaf70498750f5b04e1e22b7f4d4fdcbbc57ca9
angelomaglasang <53499928+angelomaglasang@users.noreply.github.com>
2019-08-12 08:26:16 -0700

    Update photo 3.php

--- photo 3.php
+++ photo 3.php
@@ -3,6 +3,6 @@
 <head><center><h1>MaglasangA Photo Gallery</h1></center></head>
 <body>
 <center><img src="photo 3.png" alt="Use data to see photos"></center></br>
-<left><a href="home page.php">Back to Home</a></left>
+<left><a href="index.php">Back to Home</a></left>
 </body>
 </html>
