bb520a262f26a3861d7e1fc5f8cefc448a43f351
angelomaglasang <53499928+angelomaglasang@users.noreply.github.com>
2019-08-12 08:18:03 -0700

    Update and rename home page.html to home page.php

--- home page.php
+++ home page.php
@@ -4,8 +4,8 @@
 <body>
 <h1><center>MaglasangA Photo Gallery</center></h1>
 <h1><center>LIST OF PHOTOS</center><h1>
-<center><a href="photo 1.html">Photo 1</a></center>
-<center><a href="photo 2.html">Photo 2</a></center>
-<center><a href="photo 3.html">Photo 3</a></center>
+<center><a href="photo 1.php">Photo 1</a></center>
+<center><a href="photo 2.php">Photo 2</a></center>
+<center><a href="photo 3.php">Photo 3</a></center>
 </body>
 </html>
