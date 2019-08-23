fc71d48c815ba743266f6e0b21e0ce180f99ca34
angelomaglasang <53499928+angelomaglasang@users.noreply.github.com>
2019-08-23 05:31:02 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!Doctype html>
+<html>
+
+<body>
+ <form action="photo gallery.html">
+
+    <center><h1>Login</h1>
+
+	Username: <input type="text" name="Username"><br/><br/>
+
+	Password: <input type="password" name="secure"><br/><br/>
+
+	<input type="submit" value="Sign in"></center><br/>
+</form>
+
+<center><a href="forgot password.html"><i><u>Forgot Password</u></i></a></center><br/>
+
+<center><a href="sign up.html"><i><u>Sign up</u></i></a></center></br>
+
+</body>
+
+</html>
